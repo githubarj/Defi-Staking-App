71833b925b49270b76a655d54aa0a39e57bc227d
githubarj <githuba@GitthubaRJ.localdomain>
2022-07-30 17:04:28 +0300

    no comment

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 // stake: people to lock tokens into our smart contract
 // waithdraw: unlock tokens and pull out of the contract
@@ -13,20 +13,40 @@ error Staking__transferFailed();
 
 contract Staking {
     IERC20 public s_stakingToken;
+    uint256 public constant REWARD_RATE = 100;
     uint256 public s_totalSupply;
+    uint256 public s_rewardTokenPerTokenStored;
+    uint256 public s_lastUpdateTime;
 
     //mapping someone's address to how much they have staked
     mapping(address => uint256) public s_balances;
-
+    
     modifier updateReward (address account) {
         // how mcu the reward per token is
         // last timestamp
         // 12 - 1 user earned tokens
+
+        s_rewardTokenPerTokenStored = rewardPerToken();
+        s_lastUpdateTime = block.timestamp;
+        s_rewards[account] = earned(account);
+        _;
     }
+
 
     //the staking token
     constructor(address stakingToken) {
         s_stakingToken = IERC20(stakingToken);
+    }
+
+    function earned(address account) public view returns (uint256) {
+        uint256 currentBalance = s_balances[account];
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (s_totalSupply == 0){
+            return s_rewardTokenPerTokenStored;
+        }
+        return s_rewardTokenPerTokenStored + (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18)/s_totalSupply);
     }
 
     //just one ERC20 token
